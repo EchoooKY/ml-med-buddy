a827727a847cd28c3d340f332590c18a5a51d157
EchoooKY <ky15157299776@gmail.com>
2026-07-02 10:31:55 -0700

    implement backend

---
+++ backend/main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from backend.predictor import predict
+
+app=FastAPI(
+    title="Heart Disease Prediction API",
+    version="1.0.0"
+)
+# input schema matching training features
+class HeartDieaseInput(BaseModel):
+    age:int
+    sex: int
+    cp:int
+    trestbps: float
+    chol: float
+    fbs: int
+    restecg: int
+    thalach: float
+    exang: int
+    oldpeak: float
+    slope: int
+    ca: int
+    thal: int
+
+@app.get("/health")
+def health_check():
+    return {"status":"ok"}
+
+#heart diease prediction endpoint
+@app.post("/predict-heart-disease")
+def predict_heart_disease(input_data:HeartDieaseInput):
+    result=predict(input_data=input_data.model_dump())
+    return{
+        "prediction":result["prediction"],
+        "probability":result["probability"],
+        "diagnosis":(
+            "Heart Disease Detected"
+            if result["prediction"]==1
+            else "No Heart Disease Detected"
+        )
+    }
